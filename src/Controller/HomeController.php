a0dc8a194b4326a22ccbd8c96c924bacfd5a52b3
Yurnaasu <manal.ferdaoui@etu.univ-st-etienne.fr>
2026-01-04 20:50:15 +0100

    Fi

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/projets', name: 'app_projets')]
+    public function projets(): Response
+    {
+        return $this->render('home/projets.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig');
+    }
+}
