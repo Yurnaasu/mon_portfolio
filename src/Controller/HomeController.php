2805fa1c096ddb5084ee44860bac078003546761
Yurnaasu <manal.ferdaoui@etu.univ-st-etienne.fr>
2026-01-09 11:59:52 +0100

    final

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,20 +4,32 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route; 
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')] // On met '/' pour que ce soit la page par défaut
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
     }
+    #[Route('/en-savoir-plus', name: 'app_about_more')]
+public function aboutMore(): Response
+{
+    
+    return $this->render('home/about_more.html.twig');
+}
 
-    #[Route('/projets', name: 'app_projets')]
-    public function projets(): Response
+    #[Route('/competences', name: 'app_competences')]
+    public function competences(): Response
     {
-        return $this->render('home/projets.html.twig');
+        return $this->render('home/competences.html.twig');
+    }
+
+    #[Route('/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('home/cv.html.twig');
     }
 
     #[Route('/contact', name: 'app_contact')]
@@ -25,4 +37,9 @@ class HomeController extends AbstractController
     {
         return $this->render('home/contact.html.twig');
     }
+    #[Route('/presentation', name: 'app_presentation')]
+public function presentation(): Response
+{
+    return $this->render('home/presentation.html.twig');
+}
 }
